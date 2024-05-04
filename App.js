f8e20b92dffd9b11e944527ddf6742e87641546d
iliyaaan <152992886+iliyaaan@users.noreply.github.com>
2024-05-04 11:31:32 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NoteContainer from './Components/NoteContainer/NoteContainer';
+import './Assets/GridImg.jpeg'
+
+
+function App() {
+  return (
+    <div className='App'>
+    <NoteContainer/>
+    </div>
+  );
+}
+
+export default App;
